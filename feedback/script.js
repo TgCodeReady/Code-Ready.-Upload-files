abf39fcbed7e2fc2e58b92e7bd1ed4c22f2a034f
Artem Mitkin <121829428+Finenno@users.noreply.github.com>
2024-06-19 13:14:52 +0300

    Add files via upload

---
+++ feedback/script.js
@@ -0,0 +1,14 @@
+let modal = document.getElementById('modal');
+let btn = document.getElementById("open-modal");
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
