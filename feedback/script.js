8f4958b80f6b016639bf1ab137caa305a59f091d
Artem Mitkin <121829428+Finenno@users.noreply.github.com>
2024-06-19 13:47:02 +0300

    Update script.js

--- feedback/script.js
+++ feedback/script.js
@@ -1,14 +1,32 @@
-let modal = document.getElementById('modal');
-let btn = document.getElementById("open-modal");
-let span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+	let modal = document.querySelector('.modal');
+	let openModalBtn = document.getElementById('open-modal');
+	let closeModalBtn = document.querySelector('.close');
+	let form = document.querySelector('form');
+
+	openModalBtn.addEventListener('click', function() {
+	  modal.classList.add('show');
+	});
+
+	closeModalBtn.addEventListener('click', function() {
+	  modal.classList.remove('show');
+	  modal.classList.add('hide');
+	  setTimeout(function() {
+	    modal.classList.remove('hide');
+	  }, 500);
+	});
+
+	form.addEventListener('submit', function(event) {
+	  event.preventDefault();
+	  modal.classList.add('hide');
+	  setTimeout(function() {
+	    modal.classList.remove('show');
+	    modal.classList.remove('hide');
+	  }, 500);
+	  modal.querySelector('.modal-content').style.animation = 'scaleOut 0.5s ease-out forwards';
+	});
+
+	modal.addEventListener('transitionend', function() {
+	  if (!modal.classList.contains('show')) {
+	    modal.style.display = 'none';
+	  }
+	});
